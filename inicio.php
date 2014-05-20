33eef87c6446f23a02be1dc60335206c974a6026
Rogelio <rogelio.norisc@gmail.com>
2014-05-20 11:32:28 -0600

    Inicio

--- inicio.php
+++ inicio.php
@@ -1,31 +1,23 @@
 <?php 
     $error=false; 
     include_once "FIMAZConfig.php";
+    include_once "Votos.php";
+    $votos = new Votos();
 ?>
 
 <?php include("encabezado.php"); ?>
-<div class="login panel">	
-      <form class="form-signin" method="post" action="">
-    	<?php  if($error==1){ ?>
-        	<div class="alert alert-error"><button type="button" class="close" data-dismiss="alert">&times;</button>El usuario o contraseña no es valido.</div>
-        <?php } ?> 
-    	<?php  if($error==2){ ?>
-        	<div class="alert alert-error"><button type="button" class="close" data-dismiss="alert">&times;</button>El captcha introducido no es valido.</div>
-        <?php } ?>               
-        <h2 class="form-signin-heading">Acceso de Alumnos</h2>
-        <!-- Text input-->
-        <div class="control-group">
-            <input name="txtUsuario" type="text" class="input-block-level" placeholder="usuario" data-validation-required-message="Usuario requerido." required>
-            <p class="help-block"></p>
-        </div>        
-        <div class="control-group">
-	        <input name="txtPassword" type="password" class="input-block-level" placeholder="contraseña" data-validation-required-message="Contraseña requerida."  required>
-            <p class="help-block"></p>            
-        </div>                       
-        <label class="checkbox">
-          <!--<input name="chkRecordar" type="checkbox" value="remember-me"> Recordarme-->
-        </label>
-        <button class="btn btn-large btn-primary" type="submit">Entrar</button>
-      </form>
-</div>
+<section class="columns">
+    <section class="large-12 rows lugares">
+        <div class="granador">
+            <img src="" alt="">
+            <span class="nombre"></span>
+        </div>
+    </section>
+    <section class="large-6 rows lugares">
+        
+    </section>
+    <section class="large-6 rows lugares">
+        
+    </section>
+</section>
 <?php include("piepagina.php"); ?>
