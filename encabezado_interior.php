3fa80c7fbcfd4567091f430ad2ddda2f86c08dd9
Rogelio <rogelio.norisc@gmail.com>
2014-05-20 12:52:35 -0600

    encabezado borrado

--- encabezado_interior.php
+++ encabezado_interior.php
@@ -13,20 +13,3 @@
     <script src="<?=$ruta;?>lib/modernizr.js"></script>
   </head>
   <body>
-    <header>
-      <section class="row header-bg">
-        <section class="small-3 medium-3 large-3 columns">
-          <a href="/" class="logo">Universidad Autonoma de Sinaloa</a>
-        </section>
-        <section class="small-9 medium-9 large-9 columns">
-            <h3 class="titulo-1">UNIVERSIDAD AUTÓNOMA DE SINALOA</h3>
-            <h2 class="titulo-2">FACULTAD DE INFORMATICA MAZATLÁN</h2>                        
-            <h2 class="titulo-2">VOTACIONES DÍA DEL ESTUDIANTE</h2>                        
-            <?php 
-                $dias=array("","Lunes","Martes","Miercoles","Jueves","Viernes","Sabado","Domingo");
-                $meses=array("","Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre");
-             ?>
-            <span class="fecha"><?php echo $dias[date('N')].", ".date('j')." de ".$meses[date('n')]." de ".date('Y'); ?></span>          
-        </section>
-      </section>
-    </header>
