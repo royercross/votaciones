a75cf309173c9adb913f887e4b9f77d0e25b25ae
Rogelio <rogelio.norisc@gmail.com>
2014-05-20 19:47:39 -0600

    v2.1.2

--- guardafbid.php
+++ guardafbid.php
@@ -3,6 +3,9 @@ include_once "check.php";
 include_once "Votos.php";
 $votos = new Votos();
 $input = $_POST;
+
+print_r($_POST);
+
 if(isset($input['fbid'])){
 	$me = $votos->guardaFB($input['fbid']);	
 	header("Location: inicio.php");
