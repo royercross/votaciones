3b15593f320ffb5f1c0a9c031e2673c4b7e235b1
Rogelio <rogelio.norisc@gmail.com>
2014-05-20 19:04:16 -0600

    v2.0.5

--- login.php
+++ login.php
@@ -17,6 +17,10 @@ if(isset($_POST['user'])){
 			$me = $votos->me();
 			//print_r($me);			
 			session_start();
+			$_SESSION['botonfacebook']=true;	
+			if(strlen($me->fbid) > 3){
+				$_SESSION['botonfacebook']=false;	
+			}
 			$_SESSION['TOKEN']="38f24e6d23cd0ba120f905151e91c20769f0c9e5149c111591f666f1503212e6";
 			$_SESSION['REMOTE_IP']=$_SERVER['REMOTE_ADDR'];			
 			$_SESSION['id_alumno']=$me->id_alumno;
