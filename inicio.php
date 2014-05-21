679c66668202255ff6b80f0ca95e8702113c3526
royercross <rogelio_noris@hotmail.com>
2014-05-21 00:17:13 -0600

    v3.9

--- inicio.php
+++ inicio.php
@@ -11,7 +11,7 @@
     $faltantes=0;
     $masvotadosH=$mysql->count();
     $masVotadosH=array();  
-    if($masvotados > 0){
+    if($masvotadosH > 0){
         while($row=$mysql->getRow()){
             array_push($masVotadosH, array("sexo" => $row['sexo'], "total" => $row['total'], "nombre" => $row['nombre'], "apellido_paterno" => $row['apellido_paterno'], "apellido_materno" => $row['apellido_materno'], "fbid" => $row['fbid']));
         }
@@ -24,23 +24,45 @@
     for ($i=0; $i < $faltantes; $i++) { 
         array_push($masVotadosH, array("total" => 0 , "nombre" => "?", "apellido_paterno" => "?", "apellido_materno" => "?"));
     }
+
+    $query="SELECT count(id_voto) as total, b.nombre, b.apellido_paterno, b.apellido_materno,b.sexo,b.fbid FROM votos a INNER JOIN alumnos b ON a.id_votado=b.id_alumno WHERE b.sexo='Femenino' GROUP BY id_votado ORDER BY total DESC";   
+    $mysql->execute($query);
+    $faltantes=0;
+    $masvotadosM=$mysql->count();
+    $masVotadosM=array();  
+    if($masvotadosM > 0){
+        while($row=$mysql->getRow()){
+            array_push($masVotadosM, array("sexo" => $row['sexo'], "total" => $row['total'], "nombre" => $row['nombre'], "apellido_paterno" => $row['apellido_paterno'], "apellido_materno" => $row['apellido_materno'], "fbid" => $row['fbid']));
+        }
+        
+    }
+    if($masvotadosM < 3){
+        $faltantes=3-$masvotadosM;
+    }    
+
+    for ($i=0; $i < $faltantes; $i++) { 
+        array_push($masVotadosM, array("total" => 0 , "nombre" => "?", "apellido_paterno" => "?", "apellido_materno" => "?"));
+    }    
     //print_r($masVotados);
 ?>
 
 <?php include("encabezado_interior.php"); ?>
 <?php include("menu.php"); ?>
 <section class="columns votantes-wrapper">
-    <h2 class="titulo">Los más votados</h2>
-    <?php
-        $cont=0;
-        foreach($masVotadosH as $votante){
+        <section class="large-12 rows lugares">
+            <h2 class="titulo">Los más votados</h2>            
+        </section>
+        <section class="large-12 rows lugares">
+            <h2 class="titulo-l">Hombres</h2>            
+            <h2 class="titulo-r">Mujeres</h2>            
+        </section>        
+    <?php        
+        for($i=0; $i < 3; $i++){
+        //foreach($masVotadosH as $votante){
+            $votante=$masVotadosH[$i];
             $nombre_completo = $votante['nombre']." ".$votante['apellido_paterno']." ".$votante['apellido_materno'];
-            if($cont%2==0){
     ?>
-            <section class="large-12 rows lugares">
-    <?php
-            }
-    ?>
+        <section class="large-12 rows lugares">
         <div class="ganador">
             <?php 
                 if(isset($votante['fbid']) && strlen($votante['fbid']) > 4){
@@ -56,12 +78,25 @@
             <span class="nombre"><?=$nombre_completo;?></span>
         </div>
     <?php
-            if($cont%2==1){
+        $votante=$masVotadosM[$i];
+        $nombre_completo = $votante['nombre']." ".$votante['apellido_paterno']." ".$votante['apellido_materno'];
     ?>
+        <div class="ganador">
+            <?php 
+                if(isset($votante['fbid']) && strlen($votante['fbid']) > 4){
+            ?>
+                <img src="https://graph.facebook.com/v2.0/<?=$votante['fbid'];?>/picture?type=large" alt="" />
+            <?php
+                }else{
+            ?>
+            <img src="imagenes/facebook.jpg" alt="" />
+            <?php
+                }
+            ?>
+            <span class="nombre"><?=$nombre_completo;?></span>
+        </div>        
         </section>
-    <?php
-            }
-            $cont++;
+    <?php            
         }
     ?>
     
